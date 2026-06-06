56856aca3e62641ba20bdc8d72f05cf87a5d32f5
Rolajraph <raphaellawrence078@gmail.com>
2026-06-06 21:39:10 +0100

    Fix migration conflict

--- build.sh
+++ build.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
-
-# Exit on error
+# exit on error
 set -o errexit
 
-# Install dependencies
-pip install -r requirements.txt
-
-# Collect static files (needed for your CSS/images)
-python manage.py collectstatic --no-input
-
-# Apply database migrations
+# Apply migrations
+python manage.py migrate --fake blog 0004_auto_20260606_1439
 python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --no-input
