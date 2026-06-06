90cbadd795025bf63b6e52cb146bac57b9015306
Rolajraph <raphaellawrence078@gmail.com>
2026-06-06 21:44:23 +0100

    Add pip install to build script

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 # exit on error
 set -o errexit
 
+# Install dependencies
+pip install -r requirements.txt
+
 # Apply migrations
 python manage.py migrate --fake blog 0004_auto_20260606_1439
 python manage.py migrate
